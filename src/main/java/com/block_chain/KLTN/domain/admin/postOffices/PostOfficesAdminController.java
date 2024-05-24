88218156ddd98cbf3b22fe9e62ea5b813eef6c4d
TonND-6572 <toiiuvn111@gmail.com>
2024-05-24 23:21:11 +0700

    update postoffice api

--- src/main/java/com/block_chain/KLTN/domain/admin/postOffices/PostOfficesAdminController.java
+++ src/main/java/com/block_chain/KLTN/domain/admin/postOffices/PostOfficesAdminController.java
@@ -6,15 +6,18 @@ import com.block_chain.KLTN.domain.postOffices.*;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/admin/post-offices")
@@ -36,6 +39,11 @@ public class PostOfficesAdminController {
     @GetMapping("")
     public Page<PostOfficesResponse> searchPostOffices(PostOfficesSearchRequest request, Pageable pageable) {
         return postOfficesQueryService.searchPostOffices(request, pageable);
+    }
+
+    @PutMapping("/")
+    public PostOfficesResponse updatePostOffices(@PathVariable("id") Long id, @RequestBody @Valid UpdatePostOfficesRequest request) {
+        return postOfficesService.update(id, request);
     }
 
     @PatchMapping("/{id}")
